be6cec9bc99f9f1d1a2663b2204a13605713d040
viniciusdutra314 <dutravinisousa2@usp.br>
2026-02-04 18:26:53 -0300

    feat: minimal example

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use nix::unistd;
+use std::path::{self, Path, PathBuf};
+use std::{ffi::CString, panic::UnwindSafe};
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut line = String::new();
+    loop {
+        stdin.read_line(&mut line).unwrap();
+        let mut line_it = line.split(" ");
+        let exec = line_it.next().unwrap();
+        let absolute_exec = std::env::var("PATH")
+            .unwrap()
+            .split(':')
+            .map(PathBuf::from)
+            .find_map(|mut path| {
+                path.push(exec.trim());
+                if path.is_file() { Some(path) } else { None }
+            })
+            .unwrap();
+        let fork = unsafe { unistd::fork().unwrap() };
+        match fork {
+            unistd::ForkResult::Child => {
+                let c_path = CString::new(absolute_exec.as_os_str().as_encoded_bytes()).unwrap();
+                unistd::execv(&c_path, &[&c_path]).unwrap();
+            }
+            unistd::ForkResult::Parent { child: _, .. } => {
+                nix::sys::wait::wait().unwrap();
+            }
+        }
+    }
+}
