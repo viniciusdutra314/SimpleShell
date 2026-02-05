6ce1f28700a7381717ef5d929a08f86c6211ca89
viniciusdutra314 <dutravinisousa2@usp.br>
2026-02-05 19:49:20 -0300

    feat: added cd support

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,7 @@ fn main() {
             .iter()
             .position(|&c| c == 0)
             .unwrap_or(hostname_buf.len());
-        CString::new(&hostname_buf[..len])
-            .unwrap_or_else(|_| CString::new("hostname_not_found").unwrap())
+        CString::new(&hostname_buf[..len]).unwrap()
     } else {
         CString::new("hostname_not_found").unwrap()
     };
@@ -55,39 +54,56 @@ fn main() {
 
         let mut parts = trimmed_line.split_whitespace();
         let command = parts.next().unwrap();
-        let absolute_exec = std::env::var("PATH")
-            .unwrap()
-            .split(':')
-            .map(PathBuf::from)
-            .find_map(|mut path| {
-                path.push(command);
-                if path.is_file() { Some(path) } else { None }
-            });
 
-        let exec_path = match absolute_exec {
-            Some(path) => path,
-            None => {
-                eprintln!("{}: command not found", command);
-                continue;
+        match command {
+            "exit" => return (),
+            "cd" => {
+                if let Some(path) = parts.next() {
+                    let path_c = CString::new(path).unwrap();
+                    let path_pathbuf = PathBuf::from(path);
+                    if path_pathbuf.is_dir() {
+                        unsafe { libc::chdir(path_c.as_ptr()) };
+                    } else {
+                        println!("No such file or directory {}", path);
+                    }
+                }
             }
-        };
+            _ => {
+                let absolute_exec = std::env::var("PATH")
+                    .unwrap()
+                    .split(':')
+                    .map(PathBuf::from)
+                    .find_map(|mut path| {
+                        path.push(command);
+                        if path.is_file() { Some(path) } else { None }
+                    });
 
-        unsafe {
-            let pid = libc::fork();
-            if pid == 0 {
-                let c_path = CString::new(exec_path.as_os_str().as_bytes()).unwrap();
-                let mut c_args: Vec<CString> =
-                    parts.map(|arg| CString::new(arg).unwrap()).collect();
-                c_args.insert(0, c_path.clone());
-                let mut argv: Vec<*const libc::c_char> =
-                    c_args.iter().map(|s| s.as_ptr()).collect();
-                argv.push(ptr::null());
-                libc::execv(c_path.as_ptr(), argv.as_ptr());
-            } else if pid > 0 {
-                let mut status: libc::c_int = 0;
-                libc::waitpid(pid, &mut status, 0);
-            } else {
-                eprintln!("fork failed");
+                let exec_path = match absolute_exec {
+                    Some(path) => path,
+                    None => {
+                        eprintln!("{}: command not found", command);
+                        continue;
+                    }
+                };
+
+                unsafe {
+                    let pid = libc::fork();
+                    if pid == 0 {
+                        let c_path = CString::new(exec_path.as_os_str().as_bytes()).unwrap();
+                        let mut c_args: Vec<CString> =
+                            parts.map(|arg| CString::new(arg).unwrap()).collect();
+                        c_args.insert(0, c_path.clone());
+                        let mut argv: Vec<*const libc::c_char> =
+                            c_args.iter().map(|s| s.as_ptr()).collect();
+                        argv.push(ptr::null());
+                        libc::execv(c_path.as_ptr(), argv.as_ptr());
+                    } else if pid > 0 {
+                        let mut status: libc::c_int = 0;
+                        libc::waitpid(pid, &mut status, 0);
+                    } else {
+                        eprintln!("fork failed");
+                    }
+                }
             }
         }
     }
